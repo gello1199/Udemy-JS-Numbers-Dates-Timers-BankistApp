328b6ca389a0ce963d737812374f632a87283805
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-05-11 23:28:55 -0400

    started math and rounding

--- starter/script.js
+++ starter/script.js
@@ -232,40 +232,43 @@ btnSort.addEventListener('click', function(e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+// Math and Rounding
+
 // Converting and Checking numbers
-console.log(23 === 23.0)
+// console.log(23 === 23.0)
 
-// Base 10 - 0 to 9
-// Binary base 2 - 0 1
+// // Base 10 - 0 to 9
+// // Binary base 2 - 0 1
 
-console.log(0.1 + 0.2)
-console.log(0.1 + 0.2 === 0.3)
+// console.log(0.1 + 0.2)
+// console.log(0.1 + 0.2 === 0.3)
 
-// converts strings to numbers
-console.log(Number('23'))
-console.log(+'23')
+// // converts strings to numbers
+// console.log(Number('23'))
+// console.log(+'23')
 
-// Parsing
-console.log(Number.parseInt('30px', 10))
-console.log(Number.parseInt('e23', 10))
+// // Parsing
+// console.log(Number.parseInt('30px', 10))
+// console.log(Number.parseInt('e23', 10))
 
-console.log(Number.parseInt('2.5rem'))
-console.log(Number.parseFloat('2.5rem'))
+// console.log(Number.parseInt('2.5rem'))
+// // Whenever need to read the value out of a string i.e. from CSS
+// console.log(Number.parseFloat('2.5rem'))
 
-// console.log(parseFloat('2.5rem'))
+// // console.log(parseFloat('2.5rem'))
 
-// Check if value is NaN
-console.log(Number.isNaN(20))
-console.log(Number.isNaN('20'))
-console.log(Number.isNaN(+'20x'))
-console.log(Number.isNaN(23 / 0))
+// // Check if value is NaN
+// console.log(Number.isNaN(20))
+// console.log(Number.isNaN('20'))
+// console.log(Number.isNaN(+'20x'))
+// console.log(Number.isNaN(23 / 0))
 
-// Better way to check if a value is a number
-console.log(Number.isFinite(20))
-console.log(Number.isFinite('20'))
-console.log(Number.isFinite(+'20X'))
-console.log(Number.isFinite(23 / 0))
+// // Better way to check if a value is a number
+// console.log(Number.isFinite(20))
+// console.log(Number.isFinite('20'))
+// console.log(Number.isFinite(+'20X'))
+// console.log(Number.isFinite(23 / 0))
 
-console.log(Number.isInteger(23))
-console.log(Number.isInteger(23.0))
-console.log(Number.isInteger(23 / 0))
+// console.log(Number.isInteger(23))
+// console.log(Number.isInteger(23.0))
+// console.log(Number.isInteger(23 / 0))
