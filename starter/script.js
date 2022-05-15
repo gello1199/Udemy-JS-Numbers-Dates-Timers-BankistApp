e186f374a7748c37398c6e61c7474d43327ad78e
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-05-15 17:53:55 -0400

    Numeric separators notes

--- starter/script.js
+++ starter/script.js
@@ -232,33 +232,51 @@ btnSort.addEventListener('click', function(e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+// Numeric Separators
+
+// 287,460,000,000
+const diameter = 287_460_000_000
+console.log(diameter)
+
+const price = 345_99
+console.log(price)
+
+const transferFee1 = 15_00
+const transferFee2 = 1_500
+
+// numeric separators much be between numbers
+const PI = 3.1415
+console.log(PI)
+
+console.log(Number('230_000'))
+console.log(Number('230000'))
+
+
 // The remainder operator
 
-console.log(5 % 2) // gives the remainder of 1
-console.log(5 / 2)
+// console.log(5 % 2) // gives the remainder of 1
+// console.log(5 / 2)
 
-console.log(8 % 3)
-console.log(8 / 3) // 8 = 2 * 3 + 2
+// console.log(8 % 3)
+// console.log(8 / 3) // 8 = 2 * 3 + 2
 
-console.log(6 % 2)
-console.log(6 / 2)
+// console.log(6 % 2)
+// console.log(6 / 2)
 
-console.log(7 % 2)
-console.log(7 / 2)
+// console.log(7 % 2)
+// console.log(7 / 2)
 
-const isEven = n => n % 2 === 0
-console.log(isEven(8))
-console.log(isEven(23))
-console.log(isEven(514))
+// const isEven = n => n % 2 === 0
+// console.log(isEven(8))
+// console.log(isEven(23))
+// console.log(isEven(514))
 
-labelBalance.addEventListener('click', function() {
-  [...document.querySelectorAll('.movements__row')].forEach(function(row, i) {
-    if(i % 2 === 0) row.style.backgroundColor = 'orangered' 
-    if(i % 3 === 0) row.style.backgroundColor = 'blue'
-  })
-})
-
-
+// labelBalance.addEventListener('click', function() {
+//   [...document.querySelectorAll('.movements__row')].forEach(function(row, i) {
+//     if(i % 2 === 0) row.style.backgroundColor = 'orangered' 
+//     if(i % 3 === 0) row.style.backgroundColor = 'blue'
+//   })
+// })
 
 // Math and Rounding
 
