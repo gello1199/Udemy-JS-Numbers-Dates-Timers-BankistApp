3e332468905df5804f8acc988fc2ea25f02426a4
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-05-16 19:23:27 -0400

    Creating dates notes

--- starter/script.js
+++ starter/script.js
@@ -232,44 +232,85 @@ btnSort.addEventListener('click', function(e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+// Creating Dates
+
+// Create a Date
+// const now = new Date()
+// console.log(now)
+
+// console.log(new Date('May 16 2022 17:41:32'))
+// console.log(new Date('December 24, 2015'))
+// console.log(new Date(account1.movementsDates[0]))
+
+// // month is zero based
+// console.log(new Date(2037, 10, 19, 15, 23, 5))
+// console.log(new Date(2037, 10, 33))
+
+// console.log(new Date(0))
+// console.log(new Date(3 * 24 * 60 * 60 * 1000))
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23)
+console.log(future)
+console.log(future.getFullYear())
+console.log(future.getMonth())
+console.log(future.getDate())
+// Day is day of the week
+console.log(future.getDay())
+console.log(future.getHours())
+console.log(future.getMinutes())
+console.log(future.getSeconds())
+console.log(future.toISOString())
+console.log(future.getTime())
+
+// amount of milliseconds that passed since Jan 1, 1970
+console.log(new Date(2142274980000))
+// timestamp for right now
+console.log(Date.now())
+
+// setting new date
+future.setFullYear(2040)
+console.log(future)
+
+
 // Working with BigInt
 
-console.log(2 ** 53 - 1)
-console.log(Number.MAX_SAFE_INTEGER)
-console.log(2 ** 53 + 1)
-console.log(2 ** 53 + 2)
-console.log(2 ** 53 + 3)
-console.log(2 ** 53 + 4)
+// console.log(2 ** 53 - 1)
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(2 ** 53 + 1)
+// console.log(2 ** 53 + 2)
+// console.log(2 ** 53 + 3)
+// console.log(2 ** 53 + 4)
 
-// n transforms a regular number into a BigInt number
-console.log(234907543907543223143264675n)
-// or by using BigInt function
-console.log(BigInt(234907543907543223143264675))
+// // n transforms a regular number into a BigInt number
+// console.log(234907543907543223143264675n)
+// // or by using BigInt function
+// console.log(BigInt(234907543907543223143264675))
 
-// Operations
-console.log(10000n + 10000n)
-console.log(321475438285748392n * 1000000000n)
-// console.log(Math.sqrt(16n))
+// // Operations
+// console.log(10000n + 10000n)
+// console.log(321475438285748392n * 1000000000n)
+// // console.log(Math.sqrt(16n))
 
-// can't mix BigInt with regular numbers
-const huge = 3253436548972128374n
-const num = 23
-// console.log(huge * num)
-console.log(huge * BigInt(num))
+// // can't mix BigInt with regular numbers
+// const huge = 3253436548972128374n
+// const num = 23
+// // console.log(huge * num)
+// console.log(huge * BigInt(num))
 
-// Exceptions
-console.log(20n > 15)
-console.log(20n === 20)
-console.log(typeof 20n)
-console.log(20n == 20)
-console.log(20n == '20')
+// // Exceptions
+// console.log(20n > 15)
+// console.log(20n === 20)
+// console.log(typeof 20n)
+// console.log(20n == 20)
+// console.log(20n == '20')
 
-console.log(huge + ' is REALLY big')
+// console.log(huge + ' is REALLY big')
 
-// Divisions
-console.log(11n / 3n)
-console.log(10n / 3n)
-console.log(10 / 3)
+// // Divisions
+// console.log(11n / 3n)
+// console.log(10n / 3n)
+// console.log(10 / 3)
 
 // Numeric Separators
 
