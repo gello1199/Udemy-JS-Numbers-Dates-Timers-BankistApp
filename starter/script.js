565f14a55207db5aff801e030d53318edf6b005e
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-05-20 23:29:04 -0400

    internationalizing dates using the new JS international api

--- starter/script.js
+++ starter/script.js
@@ -81,7 +81,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const formatMovementDate = function(date) {
+const formatMovementDate = function(date, locale) {
   const calcDaysPassed = (date1, date2) => 
   Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24))
   // console.log(date)
@@ -93,10 +93,11 @@ const formatMovementDate = function(date) {
   if(daysPassed === 1) return 'Yesterday'
   if(daysPassed <= 7) return `${daysPassed} days ago`
  
-    const day = `${date.getDate()}`.padStart(2, 0)
-    const month = `${date.getMonth() + 1}`.padStart(2, 0)
-    const year = date.getFullYear()
-    return `${month}/${day}/${year}`
+    // const day = `${date.getDate()}`.padStart(2, 0)
+    // const month = `${date.getMonth() + 1}`.padStart(2, 0)
+    // const year = date.getFullYear()
+    // return `${month}/${day}/${year}`
+    return new Intl.DateTimeFormat(locale).format(date)
   
 }
 
@@ -109,7 +110,7 @@ const displayMovements = function(acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
 
     const date = new Date(acc.movementsDates[i])
-    const displayDate = formatMovementDate(date)
+    const displayDate = formatMovementDate(date, acc.locale)
 
     const html = `
       <div class="movements__row">
@@ -195,9 +196,9 @@ btnLogin.addEventListener('click', function(e) {
       hour: 'numeric',
       minute: 'numeric',
       day: 'numeric',
-      month: 'long',
+      month: 'numeric',
       year: 'numeric',
-      weekday: 'long'
+      // weekday: 'long'
     }
     // const locale = navigator.language
     // console.log(locale)
