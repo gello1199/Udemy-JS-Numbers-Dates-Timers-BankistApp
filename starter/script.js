ec34c1c19a1be707a8383ad85021def7dcb30de0
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-05-21 23:30:16 -0400

    started internationalising numbers notes

--- starter/script.js
+++ starter/script.js
@@ -292,9 +292,17 @@ btnSort.addEventListener('click', function(e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-// Internationalizing Dates (Intl)
+// Internationalizing Numbers (Intl)
 
+const num = 23587543289.23
 
+const options = {
+  
+}
+
+console.log('US:' , new Intl.NumberFormat('en-US').format(num))
+console.log('Germany:' , new Intl.NumberFormat('de-DE').format(num))
+console.log(navigator.language , new Intl.NumberFormat(navigator.language).format(num))
 
 // Operations with Dates
 
